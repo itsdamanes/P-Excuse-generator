24114d396d4dc01f6b4bafa64183eeaea4a57b8e
Ignacio <ignacio.damanes@gmail.com>
2023-06-28 19:14:19 -0400

    Buenas-practicas

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
+  let primera_excusa = ["The dog", "My grandma", "His turtle", "My bird"];
+  let segunda_excusa = ["ate", "peed", "crushed", "broke"];
+  let tercera_excusa = ["my homework", "the keys", "the car"];
+  let cuarta_excusa = [
     "before the class",
     "right on time",
     "when I finished",
@@ -20,12 +20,9 @@ window.onload = function() {
   ];
 
   let excusa =
-    who[Math.floor(Math.random() * who.length)] +
-    " " +
-    action[Math.floor(Math.random() * action.length)] +
-    " " +
-    what[Math.floor(Math.random() * what.length)] +
-    " " +
+    who[Math.floor(Math.random() * who.length)] + " " +
+    action[Math.floor(Math.random() * action.length)] + " " +
+    what[Math.floor(Math.random() * what.length)] + " " +
     when[Math.floor(Math.random() * when.length)];
 
   document.getElementById("excuse").innerHTML = excusa;
